bb511dd3d0224980d661c5453ee1771b25589993
MergeCD <97386350+MergeCD@users.noreply.github.com>
2022-06-06 19:41:17 +0200

    Update TikTok-Views-Follow-Heart-Bot.py

--- TikTok-Views-Follow-Heart-Bot.py
+++ TikTok-Views-Follow-Heart-Bot.py
@@ -101,7 +101,7 @@ def loop4():
         sleep(wait_time)
         loop4()
 
-vidUrl = "https://vm.tiktok.com/ZMNdnbDSQ/?k=1" #Change with one of your tiktok videos
+vidUrl = "https://www.tiktok.com/@reddit_story69/video/7106107605235420421?is_copy_url=1&is_from_webapp=v1" #Change with one of your tiktok videos
 
 system("clear") #If you have Windows you can use cls
 tiktokbot = pyfiglet.figlet_format("NoNameoN", font="slant")
